faf5a1feb700cc6a3afd0b2dcbcba800edf23b81
Parker Warner <plouwarner@gmail.com>
2023-08-21 16:27:40 -0600

    Add table annotation

--- src/ExpenseManager.DataAccess/Models/ExpenseCategory.cs
+++ src/ExpenseManager.DataAccess/Models/ExpenseCategory.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExpenseManager.DataAccess.Models
 {
+    [Table("expense_categories")]
     public class ExpenseCategory
     {
         [Key]
